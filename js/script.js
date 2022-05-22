c8a6949f12dc7fa43be54ca2a7cec8e8545abe9f
Aisayev Duman <duman200354@mail.ru>
2022-05-22 19:16:03 +0600

    slide show functionality done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
+const slideImages = document.getElementsByClassName('image');
 
-function sayHi() {
-    alert('hi Nazerke');
+const nextImageDelay = 3000;
+let currentImageCounter = 0;
+
+slideImages[currentImageCounter].classList.add('opacity');
+
+setInterval(nextImage, nextImageDelay);
+
+function nextImage() {
+    slideImages[currentImageCounter].classList.remove('opacity');
+    currentImageCounter = (currentImageCounter + 1) % slideImages.length;
+    slideImages[currentImageCounter].classList.add('opacity');
 }
 
-window.addEventListener('load', sayHi);
+// Zanggar 26 February
+// Nazerke 12 June
